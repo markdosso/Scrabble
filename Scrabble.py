4104d5b3b24d753e76334ddd9c2d3fc533e4005d
Maya Kew <mwaldstein05@gmail.com>
2023-01-08 16:39:17 -0800

    Add To Dos

--- Scrabble.py
+++ Scrabble.py
@@ -1,5 +1,4 @@
-print("Hello World")
-
-print("Scrabble here we come.")
-#Making my first comment
-print("what do you want to spell?")
+#Create the bag of letters 
+#Create a 2D array for the gameboard
+#Deal letters to players (doesnt actually have to go to players. just remove them from bag)
+#Place words on board, and print board
