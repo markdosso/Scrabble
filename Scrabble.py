486dbfafeee363973f276b47313c88d031c2631f
Maya Kew <mwaldstein05@gmail.com>
2023-01-10 17:32:32 -0800

    add scrabble bag

--- Scrabble.py
+++ Scrabble.py
@@ -2,3 +2,7 @@
 #Create a 2D array for the gameboard
 #Deal letters to players (doesnt actually have to go to players. just remove them from bag)
 #Place words on board, and print board
+
+Scrab_Bag = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A','B','B','C','C','D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F','G','G','G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T',
+'U', 'U', 'U', 'U', 'V', 'V']
